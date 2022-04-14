6ddd6ab36238d64202a4fcced677aaf50fd54d59
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2022-04-14 21:09:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -76,6 +76,9 @@ def main():
 #=========================================
 #  st.markdown(""" <style> .font {font-size:50px;} </style> """, unsafe_allow_html=True)
 #=========================================
+original_title = '<p style="font-family:Courier; color:Blue; font-size: 20px;">Original image</p>'
+st.markdown(original_title, unsafe_allow_html=True)
+
 
   PP2 = round(predict_proba2[0,0],2)*100
 #  st.write(" 동파안전 확률은  ",predict_proba2[0,0]*100, "% 입니다.")
